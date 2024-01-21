cac15b1e3850490fa00c5320d8c9f36ab8c10659
Raymond <rrupiah@gmail.com>
2024-01-21 09:17:28 +0000

    added server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -24,15 +24,15 @@ export default function Header() {
 					rightIcon={AiOutlineSearch}
 				/>
 			</form>
-			<Button className='w-12 h-10 lg:hidden color=gray' pill>
+			<Button className='w-12 h-10 lg:hidden' color='gray' pill>
 				<AiOutlineSearch />
 			</Button>
 			<div className='flex gap-2 md:order-2'>
-				<Button className='w-12 h-10 hidden sm:inline color=gray' pill>
+				<Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
 					<FaMoon />
 				</Button>
 				<Link to='/sign-in'>
-					<Button gradientDuoTone='purpleToBlue'>Sign In</Button>
+					<Button gradientDuoTone='purpleToBlue' outline>Sign In</Button>
 				</Link>
 				<Navbar.Toggle />
 			</div>
