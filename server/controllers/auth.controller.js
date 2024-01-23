d93e6f0222982a7af1909ed971af5130b6c8e78c
Raymond <rrupiah@gmail.com>
2024-01-23 04:02:14 +0000

    added sign in api

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -1,7 +1,9 @@
-import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
 import { errorHandler } from "../utils/error.js";
 
+// User signup
 export const signup = async (req, res, next) => {
 	const { username, email, password } = req.body;
 
@@ -27,6 +29,38 @@ export const signup = async (req, res, next) => {
 	try {
 		await newUser.save();
 		res.status(201).json({ message: "User created successfully!" });
+	} catch (err) {
+		next(err);
+	}
+};
+
+// User login
+export const signin = async (req, res, next) => {
+	const { email, password } = req.body;
+
+	if (!email || !password || email === "" || password === "") {
+		next(errorHandler(400, "All fields are required!"));
+	}
+
+	try {
+		const user = await User.findOne({ email });
+		if (!user) {
+			return next(errorHandler(404, "User not found!"));
+		}
+
+		const isMatch = bcryptjs.compareSync(password, user.password);
+		if (!isMatch) {
+			return next(errorHandler(400, "Invalid credentials!"));
+		}
+
+		const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {});
+
+		res
+			.status(200)
+			.cookie("access_token", token, {
+				httpOnly: true
+			})
+			.json(user);
 	} catch (err) {
 		next(err);
 	}
